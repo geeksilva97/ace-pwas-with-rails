bcb0f6bab30abf78029963533fcd95011739c9fa
Edy Silva <edigleyssonsilva@gmail.com>
2025-02-14 23:44:32 -0300

    add banner

--- app/javascript/controllers/banner_controller.js
+++ app/javascript/controllers/banner_controller.js
@@ -1,27 +1,60 @@
 import { Controller } from "@hotwired/stimulus"
 
-// TODO: turn this into banner_controller.js
 export default class extends Controller {
   static targets = ['offline', 'upgrade'];
 
   connect() {
-    console.log(this.offlineTarget, this.upgradeTarget)
-    this.updateStatus()
+    this.updateStatus();
+
+    navigator.serviceWorker.getRegistration().then(this.handleUpgrade.bind(this));
 
     window.addEventListener('online', this.updateStatus.bind(this))
     window.addEventListener('offline', this.updateStatus.bind(this))
+    navigator.serviceWorker.addEventListener('controllerchange', this.onControllerChange.bind(this));
   }
 
   disconnect() {
     window.removeEventListener('online', this.updateStatus.bind(this))
     window.removeEventListener('offline', this.updateStatus.bind(this))
+    navigator.serviceWorker.removeEventListener('controllerchange', this.onControllerChange.bind(this));
   }
 
   serviceWorkerUpgrade() {
-    alert('gotta upgrade this thing');
+    navigator.serviceWorker.getRegistration().then(registration => {
+      const newWorker = registration.installing || registration.waiting;
+      newWorker?.postMessage({ type: 'SKIP_WAITING' });
+    });
   }
 
   updateStatus() {
     this.offlineTarget.classList.toggle('hidden', navigator.onLine)
+  }
+
+  onControllerChange() {
+    this.upgradeTarget.classList.add('hidden');
+  }
+
+  handleUpgrade(registration) {
+    if (!registration) return;
+    let promptShown = false;
+    let shouldSkipWaiting = false;
+    const sw = registration.waiting;
+
+    if (sw && sw?.state !== 'redundant') {
+      this.upgradeTarget.classList.remove('hidden');
+      promptShown = true;
+    }
+
+    registration.addEventListener('updatefound', () => {
+      const newWorker = registration.installing;
+
+      if (!promptShown) {
+        this.upgradeTarget.classList.remove('hidden');
+      }
+
+      if (shouldSkipWaiting) {
+        newWorker.postMessage({ type: 'SKIP_WAITING' });
+      }
+    });
   }
 }
