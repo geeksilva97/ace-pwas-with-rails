fa7cd86e2fea8b3a9db0da2c97efccd4bd4ec91b
Edy Silva <edigleyssonsilva@gmail.com>
2025-02-15 00:04:19 -0300

    remove unused code

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,44 +2,9 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 
-function showConfirmationPrompt(sw) {
-  const userChoice = Boolean(confirm('A new version of this site is available. Load it?'));
-
-  if (userChoice && sw) {
-    sw.postMessage({ type: 'SKIP_WAITING' });
-  }
-
-  return userChoice;
-}
-
-function handleUpgrade(registration) {
-  if (!registration) return;
-  let promptShown = false;
-  let shouldSkipWaiting = false;
-  const sw = registration.waiting;
-
-  if (sw && sw?.state !== 'redundant') {
-    shouldSkipWaiting = showConfirmationPrompt(sw);
-    promptShown = true;
-  }
-
-  registration.addEventListener('updatefound', () => {
-    const newWorker = registration.installing;
-
-    if (!promptShown) {
-      return showConfirmationPrompt(newWorker);
-    }
-
-    if (shouldSkipWaiting) {
-      newWorker.postMessage({ type: 'SKIP_WAITING' });
-    }
-  });
-}
-
 function registerServiceWorker() {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/service-worker.js')
-      // .then(handleUpgrade)
       .catch((error) => {
         console.error('Service worker registration failed:', error);
       });
